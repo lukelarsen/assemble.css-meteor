8a4441a905c9ec38a952fcee077b98c6c5e51f54
Luke Larsen <luke@lukelarsen.com>
2015-02-12 08:55:04 -0700

    add submodule for assemble.css, requirements, and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assemble.css
+[Assemble.css](http://assemblecss.com) as a Meteor Package.
---
+++ versions.js
@@ -0,0 +1,13 @@
+{
+  "dependencies": [
+    [
+      "fourseven:sass",
+      "2.0.0_1"
+    ],
+    [
+      "meteor",
+      "1.1.3"
+    ]
+  ],
+  "pluginDependencies": []
+}
