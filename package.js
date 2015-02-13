27a76fd555c4958e02ef74254905138374e62c12
Luke Larsen <luke@lukelarsen.com>
2015-02-12 22:39:30 -0700

    update version number and truely remove unused files

--- package.js
+++ package.js
@@ -1,22 +1,12 @@
 Package.describe({
   name: 'lukelarsen:assemble.css-meteor',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'Assemble.css as a Meteor Package. See assemblecss.com for more details.',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/lukelarsen/assemble.css-meteor',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.addFiles('lukelarsen:assemble.css-meteor.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('lukelarsen:assemble.css-meteor');
-  api.addFiles('lukelarsen:assemble.css-meteor-tests.js');
+  //api.addFiles('lukelarsen:assemble.css-meteor.js');
 });
