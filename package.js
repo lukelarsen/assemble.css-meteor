f7f80d11176a3d3293a1d3952478d891b456afa3
Luke Larsen <luke@lukelarsen.com>
2015-08-28 14:01:07 -0600

    pull in changes from assemble.css

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lukelarsen:assemble.css-meteor',
-  version: '1.4.1',
+  version: '1.4.2',
   summary: 'Assemble.css as a Meteor Package. See assemblecss.com for more details.',
   git: 'https://github.com/lukelarsen/assemble.css-meteor',
 });
